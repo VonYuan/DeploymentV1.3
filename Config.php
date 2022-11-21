ecbe6059fbdea8883176e73a733e30448210a3e0
Voon Yuan <80446914+VonYuan@users.noreply.github.com>
2022-11-21 14:07:39 +0800

    Up

--- Config.php
+++ Config.php
@@ -1,22 +1,10 @@
 <?php
 session_start(); //start the session
-$host = 'gasmeter.mysql.database.azure.com';
-$username = 'gasmeter';
-$password = 'AdminLogin123';
-$db_name = 'ocawbms';
-
-//Initializes MySQLi
 $link = mysqli_init();
-
 mysqli_ssl_set($link,NULL,NULL, "/var/www/html/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-
-// Establish the connection
-mysqli_real_connect($link, 'gasmeter.mysql.database.azure.com', 'gasmeter', 'AdminLogin123', 'ocawbms', 3306, NULL, MYSQLI_CLIENT_SSL);
-
-//If connection failed, show the error
-if (mysqli_connect_errno())
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
+mysqli_real_connect($link, 'gasmeter.mysql.database.azure.com', 'gasmeter', 'AdminLogin123', 'ocawbms', 3306, MYSQLI_CLIENT_SSL);
+if (mysqli_connect_errno($link)) {
+die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
 ?>
