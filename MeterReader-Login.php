383ab38435bb5d1d66c7ed7f3768d8d6dd3f26f7
Voon Yuan <voonyuan11@gmail.com>
2022-11-23 12:31:03 +0800

    Update Connection

--- MeterReader-Login.php
+++ MeterReader-Login.php
@@ -8,16 +8,8 @@ session_start();
 //   exit;
 // }
 
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'ocawbms');
+require_once "config.php";
 
-$link = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
-if ($link === false) {
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
 
 // Define variables and initialize with empty values
 $reader_username = $reader_password = "";
