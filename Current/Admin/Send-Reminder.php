30343c0f3880cf2e220b8cc2fbae181d3b169f08
Voon Yuan <voonyuan11@gmail.com>
2022-11-30 22:53:29 +0800

    Update Send-Reminder.php

--- Current/Admin/Send-Reminder.php
+++ Current/Admin/Send-Reminder.php
@@ -24,33 +24,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     $sql = "INSERT INTO notifications (user_id, month, message) VALUES ('$user_id', '$month', '$message')";
     
     if (mysqli_query($link, $sql)){
-        try {
-            //Server settings
-            $mail->isSMTP();
-            $mail->Host = 'smtp.gmail.com';
-            $mail->SMTPAuth = true;
-            $mail->Username = "ocawbms2021@gmail.com";
-            $mail->Password = "OCAWBMS2021";
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-            $mail->Port = 587;
-
-            //Recipients
-            $mail->setFrom("ocawbms2021@gmail.com", "OCAWBMS");
-            $mail->addAddress($user_email);     // Add a recipient
-
-            // Content
-            $mail->isHTML(true);                                  // Set email format to HTML
-            $mail->Subject = 'Online Gas Bill Management System Reminders';
-
-            $mail->Body    = "<h5>Dear '$user_name',</h5><br>".$message."<br>Best Regards,<br>OEAWBMS Team";
-
-            $mail->send();
-
-        } catch (Exception $e) {
-             echo 'Something went wrong,try again later';
-        }
         header("Location: SendReminders.php?user_id=$user_id");
-        exit();
+        
     }
     else{
         echo ("Something went wrong. Please try again later!".mysqli_error($link));
