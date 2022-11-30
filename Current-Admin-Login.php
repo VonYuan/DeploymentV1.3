f28ac5e4892eb495a901144b3fc383589c7b9bb2
Voon Yuan <voonyuan11@gmail.com>
2022-12-01 00:17:08 +0800

    Update Current-Admin-Login.php

--- Current-Admin-Login.php
+++ Current-Admin-Login.php
@@ -98,7 +98,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>OCAWBMS</title>
+    <title>Petros</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
